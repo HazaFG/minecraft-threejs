c725619a52d8eaad8e4a351e6434a75027faeaf0
HazaFG <hazaelfg@gmail.com>
2026-03-14 12:06:01 -0700

    import y export de las imagenes y sus texturas

--- src/images/textures.js
+++ src/images/textures.js
@@ -1,6 +1,10 @@
 import { RepeatWrapping } from "three";
 import {
   grassImg,
+  dirtImg,
+  glassImg,
+  logImg,
+  woodImg,
 } from "./images";
 
 //Three es la biblioteca de js puro
@@ -8,9 +12,17 @@ import { TextureLoader } from "three";
 import { NearestFilter } from "three";
 
 const groundTexture = new TextureLoader().load(grassImg)
+const dirtTexture = new TextureLoader().load(dirtImg)
+const glassTexture = new TextureLoader().load(glassImg)
+const logTexture = new TextureLoader().load(logImg)
+const woodTexture = new TextureLoader().load(woodImg)
 
 groundTexture.wrapS = RepeatWrapping
 groundTexture.wrapT = RepeatWrapping
 groundTexture.magFilter = NearestFilter
+dirtTexture.magFilter = NearestFilter
+glassTexture.magFilter = NearestFilter
+logTexture.magFilter = NearestFilter
+woodTexture.magFilter = NearestFilter
 
-export { groundTexture }
+export { groundTexture, dirtTexture, glassTexture, logTexture, woodTexture }
